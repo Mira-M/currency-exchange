5afa1229b39f5256fa2b984de9b2d0bcc8d73cf6
Mira-M <mira.mollar@gmail.com>
2016-04-15 21:50:20 -0400

    Added logic for setting base and destination setting

--- view1/view1.js
+++ view1/view1.js
@@ -11,7 +11,16 @@ angular.module('myApp.view1', ['ngRoute'])
 
 .controller('View1Ctrl', [ '$log', '$scope','RestController', function($log, $scope, RestController) {
 	
-	var latest_rates = RestController.get_latest_rates('USD')
+	$scope.set_destination_country = function (destination_country){
+		$scope.destination = destination_country;
+	};
+	
+	$scope.set_base_country = function (base_country){
+		$scope.base= base_country;
+	};
+	
+/*
+		RestController.get_latest_rates(base_country)
 		.then(function(response){
 			
 			$scope.latest_rates = response.data;
@@ -19,6 +28,8 @@ angular.module('myApp.view1', ['ngRoute'])
 			$log.debug($scope.latest_rates);
 			$log.debug($scope.latest_rates.rates['AUD'])
 		});
+*/
+
 	
 	$scope.countries = RestController.countries;
 	
