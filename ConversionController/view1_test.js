dbc19bf1ff4029eca5a606844279bdd6fd2129de
Mira-M <mira.mollar@gmail.com>
2016-04-16 06:17:14 -0400

    Changed name to match Controller refactor

---
+++ ConversionController/view1_test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('myApp.ConversionController module', function() {
+
+  beforeEach(module('myApp.ConversionController'));
+
+  describe('conversion controller', function(){
+
+    it('should ....', inject(function($controller) {
+      //spec body
+      var ConversionCtrl = $controller('ConversionCtrl');
+      expect(ConversionCtrl).toBeDefined();
+    }));
+
+  });
+});
