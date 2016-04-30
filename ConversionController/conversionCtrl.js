92222c76217193bb29389fd06f0cc065f3a434f9
Mira-M <mira.mollar@gmail.com>
2016-04-30 17:15:13 -0400

    Changed how data is retrieved and added submit button and functionality

--- ConversionController/conversionCtrl.js
+++ ConversionController/conversionCtrl.js
@@ -9,52 +9,139 @@ angular.module('myApp.ConversionController', ['ngRoute'])
   });
 }])
 
-.controller('ConversionCtrl', [ '$log', '$scope','RestController', 'ViewController', function($log, $scope, RestController, ViewController) {
+.controller('ConversionCtrl', [ '$filter', '$log', '$scope','RestController', 'ViewController', function($filter, $log, $scope, RestController, ViewController) {
 	
-	$scope.set_destination_country = function (destination_country){
-		$scope.destination = destination_country;
-	};
 	
 	$scope.set_base_country = function (base_country){
 		$scope.base = base_country;
 	};
 	
-	$scope.get_conversion = function(){
-		$log.debug("Entering Get Conversion");
-		
-		if ($scope.base != undefined && $scope.destination != undefined){
-			
-			RestController.get_latest_rates($scope.base)
-				.then(function(response){
-			
-				$scope.latest_rates = response.data;
-				$scope.conversion = $scope.latest_rates.rates[$scope.destination];
-				
-				$log.debug($scope.latest_rates);
-				$log.debug($scope.conversion);
-			});
-		};	
-		
-		if ($scope.base != undefined){
-			
-			RestController.get_current_rate($scope.base)
-				.then(function(response){
-					$scope.current_rate = response.data;
-					$scope.instant_base_display = $scope.current_rate.rates[$scope.base];
-			});
-		};	
-		
-		if ($scope.destination != undefined){
-			
-			RestController.get_current_rate($scope.destination)
-				.then(function(response){
-					$scope.current_rate = response.data;
-					$scope.instant_destination_display = $scope.current_rate.rates[$scope.destination];
-			});
-		};	
+	$scope.set_destination_country = function (destination_country){
+		$scope.destination = destination_country;
 	};
 	
+	$scope.get_base_rate = function (base_country){
+		$log.debug("Entering get_base_rate()");
+		$scope.instant_base_display = undefined;
+		$scope.conversion = undefined;
+		$scope.base_display_message = undefined;
+		$scope.base_display_number = undefined;
+		
+		
+		base_country = $filter('uppercase')(base_country);
+		$log.debug(base_country);
+		
+		if (base_country !== undefined){
+			var included = $filter('filter')($scope.countries, {$:base_country});
+			$log.debug(included);
+			
+			var valid = base_country.length === 3;
+			
+			if (!valid){
+				$scope.instant_base_display = "Country Code must be 3 characters."
+			}
+			
+			if (included === undefined){
+				$scope_instant_base_display = base_country + " is not in our database."
+			}
+			
+			
+			if (included !== undefined && valid){
+				$log.debug("DEFINED");
+				
+				RestController.get_current_rate(base_country)
+					.then(function(response){
+						$scope.current_rate = response.data;
+						$log.debug('$scope.current_rate');
+						$log.debug($scope.current_rate);
+						$scope.parse_country = $scope.current_rate.rates[base_country];
+						
+						if ($scope.parse_country !== undefined){
+							$scope.instant_base_display = $scope.parse_country;
+						}
+						
+						if (angular.isNumber($scope.instant_base_display)){
+							$scope.base_display_number = 1.00;
+						} else {
+							$scope.base_display_message = "Results for this country not available."
+						}
+				});
+			};
+		};
+
+	};
+	
+	$scope.get_destination_rate = function (destination_country){
+		$log.debug("Entering get_base_rate()");
+		$scope.instant_destination_display = undefined;
+		$scope.conversion = undefined;
+		
+		destination_country = $filter('uppercase')(destination_country);
+		$log.debug(destination_country);
+		
+		if (destination_country !== undefined){
+			var included = $filter('filter')($scope.countries, {$:destination_country});
+			$log.debug(included);
+			
+			var valid = destination_country.length === 3;
+			
+			if (!valid){
+				$scope.instant_destination_display = "Country Code must be 3 characters."
+			}
+			
+/*
+			if (included === undefined){
+				$scope.instant_destination_display = base_country + " is not in our database."
+			}
+*/
+			
+			if (included !== undefined && valid){
+				$log.debug("DEFINED");
+				
+				RestController.get_current_rate(destination_country)
+					.then(function(response){
+						$scope.current_rate = response.data;
+						$log.debug('$scope.current_rate');
+						$log.debug($scope.current_rate);
+						$scope.parse_country = $scope.current_rate.rates[destination_country];
+						
+						if ($scope.parse_country !== undefined){
+							$scope.instant_destination_display = $scope.parse_country;
+						} else {
+							//THROW SOME EXCEPTION HERE, MODAL WITH SOMETHING OR SOMETHING
+							$scope.instant_destination_display = "Results for this country not available."
+						}	
+				});
+			};
+		};
+
+	};
+	
+	$scope.get_conversion = function(base, destination){
+		
+		var invalid = angular.equals(base, destination);
+		
+		if (invalid){
+			
+			$scope.conversion = "Choose a different destination country and resubmit."
+			
+		};
+		
+		if (!invalid){
+			RestController.get_latest_rates(base)
+				.then(function(response){
+			
+				var latest_rates = response.data;
+				$scope.conversion = latest_rates.rates[destination];
+				
+				
+
+			});
+		}
+		
+	};
+	
+
 	$scope.countries = ViewController.countries;
 	$scope.footer_text = ViewController.footer_text;
-	
 }]);
